4f561b274385ecec18b18f0160493a6ad583f405
Prnce Tyagi <princetyagi1901@gmail.com>
2025-02-26 22:21:28 +0530

    new

--- controllers/Post.js
+++ controllers/Post.js
@@ -143,7 +143,8 @@ exports.getAllPosts = async (req, res) => {
     const allPosts = await Post.find(
       { status: "Published" },
       {
-
+        description: true,
+        postUrl:true,
         creator: true,
         CommentsAndLike: true,
       }
